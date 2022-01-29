93d40156cce8b6ea3b0790e21d51a81a9fcb3ef8
Jared Kroke <jared.kroke@gmail.com>
2022-01-29 14:26:56 -0600

    Updated the gallery data file and added a few more images to the project

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, title: 'Small Goat', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, title: 'Great White Shark', path: 'images/greatWhiteShark.jpg', description: 'Photo of a Great White Shark drifting through the ocean.', likes: 0},
+    {id:3, title: 'Grand Canyon', path: 'images/grandCanyon.jpg', description: 'Photo of the Grand Canyon in all its natural beauty.', likes: 0},
+    {id:4, title: 'Moose', path: 'images/moose.jpg', description: 'Photo of an adult male moose in its natural habitat.', likes: 0},
+    {id:5, title: 'Eiffel Tower', path: 'images/eiffelTower.jpg', description: 'Photo of the monumental Eiffel Tower in Paris, France.', likes: 0}
 ];
 
 module.exports = galleryItems;
