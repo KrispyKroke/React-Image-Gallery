cd8e3a5df01430d184d0309beffbd87ecfa5799a
Jared Kroke <jared.kroke@gmail.com>
2022-01-30 11:55:19 -0600

    Added in conditional rendering for the display of descriptions and images, with the ability to toggle between.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,16 +1,31 @@
-function GalleryItem({galleryItem}) {
+import {useState} from 'react';
 
+
+
+function GalleryItem({galleryItem}) {
+    // Local state for handling the toggling of display of pictures and descriptions.
+    const [isClicked, setIsClicked] = useState(false);
+
+
+    // This handles the display of text for the amount of likes a photo has received.
     let likesText = '';
     if (galleryItem.likes <= 0) {
         likesText = 'No people love this :(';
     } else {
         likesText = `${galleryItem.likes} people love this!`;
     }
+    // Variables used for the conditional rendering of images and descriptions.
+    const image =  <img src={galleryItem.path} width="200" height="200"></img>;
+    const description = <p>{galleryItem.description}</p>
+    // Toggles local state when run.
+    const runSwap = () => {
+        setIsClicked(!isClicked);
+    }
 
     return (
         <div className="picture">
-            <div>
-                <img src={galleryItem.path} width="200" height="200"></img>
+            <div onClick={runSwap}>
+                {isClicked ? description : image}
             </div>
             <div>
                 <button>Love It!</button>
